838e8d0db187a1fc9d5e90917f0903c6f4f9127b
Alan Saji <alansajivalolickal@gmail.com>
2025-06-14 17:47:05 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,11 @@
+    </main>
 
+    <footer>
+        <div class="container">
+            <p>&copy; <?php echo date('Y'); ?> Used Car Marketplace. All rights reserved.</p>
+        </div>
+    </footer>
+
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
